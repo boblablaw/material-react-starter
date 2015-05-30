99d773916f0121ba1f0f922f06ff16aa2141f14b
Jason Beatty <jasondavidbeatty@gmail.com>
2015-05-30 13:22:14 -0400

    add harmony to jshint

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -3,5 +3,5 @@ var shell = require('gulp-shell');
 var handleErrors = require('../util/handleErrors');
 
 gulp.task('jshint', shell.task([
-  './node_modules/.bin/jsxhint'
+  './node_modules/.bin/jsxhint --harmony'
 ])).on('error', handleErrors);
